c283296b825fe8a426620f9adbffe7b4f89ab364
David Elias C <davidcerchiaro8@gmail.com>
2021-05-04 11:49:14 -0500

    .

--- src/componentes/ContadorComponentes.js
+++ src/componentes/ContadorComponentes.js
@@ -1,9 +1,35 @@
-import React, { useState, Component } from 'react';
+import React, { useState, Component, useEffect } from 'react';
 import Boton from './elementos/Boton';
 
 const ContadorComponentes = (props) => {
     const [contador, incrementarDisminuir] = useState(0);
+    
+    /* 
+    //se ejecuta inicia el componente
+    useEffect(() => {
+        console.log("Solo se ejecuta al inicio..");
+    },[]);
+    //se ejecuta al inicio y cuando hay un cambio en el estado del componente
+    useEffect(()=>{
+        console.log("Se ejeccuta en cada cambio..");
+    });
+    
+    
+    //solo se ejecuta cuando cambia el estado del contador..
+    useEffect(()=>{
+        console.log("cambio el contador",contador)
+    },[contador]);
+    */
+    useEffect(()=>{
+        //codigo del efecto
+        return(()=>{
+            console.log("adios componente");
+        });
+    },[]);
 
+
+    
+    //contador
     const Incrementar = (n) => {
         incrementarDisminuir(contador + n);
     }
